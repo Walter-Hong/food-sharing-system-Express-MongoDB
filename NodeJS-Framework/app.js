bf4703c1dafa6d441d5a1c516a9606cbb57f659c
WalterHong <75154166@qq.com>
2018-05-09 22:35:39 +0100

    implement register

--- NodeJS-Framework/app.js
+++ NodeJS-Framework/app.js
@@ -1,66 +1,40 @@
-/**
- * 项目总体配置
- */
-
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session');
-var flash = require('connect-flash');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-//var index = require('./routes/index');
-var login = require('./routes/login');
+
 var register = require('./routes/register');
-var crypto = require('crypto');
-var models = require('./models');
 var app = express();
 
-/**
- *session 存储配置
- */
-global.storeMemory = new session.MemoryStore({
-    reapInterval: 60000 * 10
-});
-
-/**
- * 设置view渲染引擎为jade
- */
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-/**
- * 设置中间件和静态路径
- */
-
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 //  1. 声明路由转发
 app.use('/', register);
+app.use('/login', register);
 
-
-// 捕获404事件
+// catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-// 错误处理
+// error handlers
 
-// 开发状态打印错误信息
+// development error handler
+// will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function (err, req, res, next) {
         res.status(err.status || 500);
@@ -71,7 +45,8 @@ if (app.get('env') === 'development') {
     });
 }
 
-// 部署状态隐藏错误
+// production error handler
+// no stacktraces leaked to user
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
--- NodeJS-Framework/routes/register.js
+++ NodeJS-Framework/routes/register.js
@@ -1,14 +1,9 @@
-/**
- *author walter 
- *注册页路由
- */
-
 var express = require('express');
 var router = express.Router();
-var models = require("./models");
+// var models = require("../models");
 
 // 2.1注册uri匹配进入到注册页面
-router.get('/register', function (req, res, next) {
+router.get('/', function (req, res, next) {
     res.render('register');
 });
 
@@ -17,22 +12,7 @@ router.post('/', function (req, res, next) {
     var err = false;
     var data = req.body;
     var password = data.password;
-    if (password) {
-        // 密码加盐再md5处理后保存到数据库
-        var salt = crypto.randomBytes(8);
-        var recievedHash = crypto.createHash('md5')
-            .update(password)
-            .update(salt)
-            .digest('hex');
-        var hashedPassword = recievedHash + ',' + salt.toString('hex');
-        data.password = hashedPassword;
-    }
-
-    // 根据数据创建用户
-    models.User.create(data).then(function (user) {
-        // 成功创建回到首页
-        res.render('index');
-    }).catch(next);
+    res.render('index', {title: 'register success!'});
 });
 
 module.exports = router;
