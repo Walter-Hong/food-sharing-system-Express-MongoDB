ec4fd199112d0bdd476a3030e5ad407b3bfaae96
Walter_Hong <walterhong@foxmail.com>
2018-07-09 20:16:48 +0100

    update

--- Chuhe(Express+MongoDB+VUE+Firebase)/fmdb/index.js
+++
@@ -1,5 +0,0 @@
-exports.user = require('./user');
-exports.rank = require('./rank');
-exports.topic = require('./topic');
-exports.reply = require('./reply');
-exports.topic_passed = require('./topic_passed');
--- Chuhe(Express+MongoDB+VUE+Firebase)/public/js/head_phone.js
+++
@@ -1,9 +0,0 @@
-$(window).ready(function () {
-	$('.back').on('click', function () {
-		if (document.referrer === window.location.href || document.referrer.indexOf('http://' + document.domain) === -1) {
-			window.location.href = '/';
-			return;
-		}
-		window.history.back();
-	});
-});
